557b9e93a40bcc9410cab33ccf327c1223c5fd0e
juananPROFE <juanantoniolm@gmail.com>
2024-01-31 13:37:47 +0100

    Primera versión

---
+++ sdk/build/intermediates/cxx/Debug/403x602t/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\juana\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\juana\\AndroidStudioProjects\\bocciaPRUEBA\\sdk\\.cxx\\Debug\\403x602t\\x86_64" ^
+  opencv_jni_shared
